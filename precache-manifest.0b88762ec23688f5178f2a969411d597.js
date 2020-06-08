a73cd2aed1eeb6f98861fbbc4b747dfe5f68be6f
ShihaoDong1 <shihao.dong1@gmail.com>
2020-06-07 22:41:21 -0400

    Updates

---
+++ precache-manifest.0b88762ec23688f5178f2a969411d597.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3748fb9afdf57e624741057283d3a0e8",
+    "url": "/Assignment-8-DOM-Challenges-III/index.html"
+  },
+  {
+    "revision": "806bda2571c6660c1fd3",
+    "url": "/Assignment-8-DOM-Challenges-III/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "444d01c417e44c29e1e3",
+    "url": "/Assignment-8-DOM-Challenges-III/static/js/2.11cca046.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Assignment-8-DOM-Challenges-III/static/js/2.11cca046.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "806bda2571c6660c1fd3",
+    "url": "/Assignment-8-DOM-Challenges-III/static/js/main.c38a0eb7.chunk.js"
+  },
+  {
+    "revision": "199e694de5396ca17b06",
+    "url": "/Assignment-8-DOM-Challenges-III/static/js/runtime-main.938c768e.js"
+  }
+]);
